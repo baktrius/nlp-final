167b39822b617cee623ade55ea1b2dd138f0b225
Krzysztof Rogowski <krzysztor99@gmail.com>
2023-06-10 12:36:11 +0200

    initial commit

---
+++ cmd.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/bash
+/home/kr418382/nlp2/.venv/bin/python3.8 run_mlm.py \
+    --model_name_or_path xlm-roberta-base \
+    --train_file en_data.txt \
+    --line_by_line \
+    --per_device_train_batch_size 8 \
+    --per_device_eval_batch_size 8 \
+    --do_train \
+    --output_dir tmp/test-mlm \
+    --overwrite_output_dir
